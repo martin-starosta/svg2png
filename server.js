c4f64a4425f2fec274c359f55cfd2fce874ad68a
Martin Starosta <martin.starosta@labzone.sk>
2020-05-24 12:57:48 +0200

    Cors

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const svgToImg = require('svg-to-img')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 4000
 
 app.use(bodyParser.json())
 
-// Add headers
+/* Add headers
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader(
@@ -18,7 +19,8 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true)
 
   next()
-})
+})*/
+app.use(cors())
 
 app.post('/api/convert/svg', (req, res) => {
   const svg = req.body.svg
